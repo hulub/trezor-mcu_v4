521b45f2c7a8ed49bf5aab7c0b3dcdd21ec417ce
hulub <hulub_sky_master@yahoo.com>
2016-05-09 11:53:07 +0200

    print the size of each public key

--- firmware/fsm.c
+++ firmware/fsm.c
@@ -781,6 +781,15 @@ void fsm_msgRingSignMessage(RingSignMessage *msg) {
 	// print all public keys from L
 	uint8_t i;
 	for (i = 0; i < msg->n; i++) {
+		// print size of L[i]
+		layoutNumber((uint32_t) msg->L[i].size, "size of L[" + i + "]:" );
+		if (!protectButton(ButtonRequestType_ButtonRequest_PublicKey, true)) {
+			fsm_sendFailure(FailureType_Failure_ActionCancelled,
+					"Show public key cancelled");
+			layoutHome();
+			return;
+		}
+
 		layoutPublicKey(msg->L[i].bytes);
 		if (!protectButton(ButtonRequestType_ButtonRequest_PublicKey, true)) {
 			fsm_sendFailure(FailureType_Failure_ActionCancelled,
